474c0fe849cb523dbe903fb064f2f9e4e988d167
Levi M. Luke <levi@ceelectronics.com>
2021-02-05 10:32:34 -0500

    Add TimeoutAfter Class Function || Update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# PyTimeoutAfter
-A signal free, multi-threading safe, method of setting A Python mid-function Timeout
+Timeout After (X Seconds)
+===========================
+A signal free, multi-threading safe, method of setting A mid-function Timeout
+---------------------------------------------------------------------------
+
+In an effort to find a non-blocking, minimally intrusive, replacement for the [signal method](https://www.jujens.eu/posts/en/2018/Jun/02/python-timeout-function/) of making a mid-function timeout, I stumbled upon this "Nasty hack" posted by [liuw on GitHub](https://gist.github.com/liuw/2407154), which allowed me to actual do what I originally though was impossible.
+
+As LIU points out, this is a NASTY hack and is only possible by breaking the C level interpreter to allow this to happen. This can be a point of concern and my be fixed in the future, but as for now it seems safe to use with the current version of python 3 (Tested on 3.6.5 - 3.8.0)
+
+And if I haven’t stressed this enough, I (we) take no responsibility for the use of this exploit, use at your own risk.
+
+
+References
+----------
+https://gist.github.com/liuw/2407154 (Nasty Hack)
+
+
+Example Usage
+-------------
+
+```python
+import timeout_after
+
+def test_me():
+	try:
+		with TimeoutAfter(timeout=3, exception=TimeoutError):
+			for x in range(0, 6):
+				print('Tick {}'.format(x + 1))
+				time.sleep(1)
+		print("I don't say anything...")
+	except TimeoutError:
+		print('Looks like we ran out of ticks...')
+
+thread = threading.Thread(target = test_me)
+thread.start()
+thread.join()
+```
---
+++ timeout_after.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import threading, time, ctypes
+
+'''
+-----------------------------------------------------------------------
+Copyright (c) 2021 Dr. Luke, LIU Wei, Brett Husar, and others
+-----------------------------------------------------------------------
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
+class TimeoutAfter():
+	def __init__(self, timeout=(10), exception=TimeoutError):
+		self._exception = exception
+		self._caller_thread = threading.current_thread()
+		self._timeout = timeout
+		self._timer = threading.Timer(self._timeout, self.raise_caller)
+		self._timer.daemon = True
+		self._timer.start()
+
+	def __enter__(self):
+		try:
+			yield
+		finally:
+			self._timer.cancel()
+		return self
+
+	def __exit__(self, type, value, traceback):
+		self._timer.cancel()
+		
+	def raise_caller(self):
+		ret = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(self._caller_thread._ident), ctypes.py_object(self._exception))
+		if ret == 0:
+			raise ValueError("Invalid thread ID")
+		elif ret > 1:
+			ctypes.pythonapi.PyThreadState_SetAsyncExc(self._caller_thread._ident, NULL)
+			raise SystemError("PyThreadState_SetAsyncExc failed")
